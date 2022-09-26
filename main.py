ad97e55d9aa5b3bc186ed289806ecfc93600c12d
Adam <adamfdunmore@gmail.com>
2022-09-26 05:01:30 +0100

    Added Basic Import Script and barebones for UI

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #imports
 import tkinter 
 import sqlite3 as sql
+import csv
 
 #windows
 
@@ -19,19 +20,37 @@ def screensizeGrab():
 tk.after(100, screensizeGrab)
 
 #Creates Sidebar
-sidebar = tkinter.Frame(tk, width=300,height=400, borderwidth=4, bg="grey")
-sidebar.pack(side="right")
+sidebar = tkinter.Frame(tk, width=300,height=800, borderwidth=4, bg="grey")
+sidebar.pack(side="right", fill=tkinter.Y)
 #
 # Creates updating variables
 def updates():
-    sidebar.config(height=screensizeHeight)
+    
     tk.after(1,updates)
 tk.after(101, updates)
 
 #Creates database
-def databaseCreation():
-    connection = sql.connect("clients.db")
-    cursor = connection.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS clients(firstName TEXT, lastName TEXT, email TEXT, Topics TEXT)")
-databaseCreation()
+connection = sql.connect("clients.db")
+cursor = connection.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS clients(firstName TEXT, lastName TEXT, email TEXT, Topics TEXT)")
+
+#script for importing csv files
+def importCSV():
+    with open('','r') as fin:
+        dr = csv.DictReader(fin)
+        to_db = [(i['firstName'], i['lastName'], i['email'], i['Topics']) for i in dr]
+    cursor.executemany("INSERT INTO clients (firstName, lastName, email, Topics) VALUES (?, ?, ?, ?);", to_db)
+
+#Creates imports window
+importFrame = tkinter.Frame(tk, bg="white", width=100, height=100)
+
+def ImportFrameSidebar():
+    importFrame.pack(fill=tkinter.BOTH, expand=1)
+
+importButtonSidebar = tkinter.Button(sidebar, text="Import", width=33,height=5, command=ImportFrameSidebar)
+importButtonSidebar.place(x=0,y=0)
+
+
+
+
 tk.mainloop()
