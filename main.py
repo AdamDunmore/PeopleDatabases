3a16d0271503df175fee3a73d97f60ab0cb8cf8f
Adam <adamfdunmore@gmail.com>
2022-09-26 04:28:14 +0100

    Added database

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+#imports
 import tkinter 
-import sqlite3
+import sqlite3 as sql
+
+#windows
 
 tk = tkinter.Tk()
 tk.geometry("1500x800")
 
+#Gets constant screen size
 def screensizeGrab():
     global screensize
     global screensizeWidth
@@ -13,11 +17,21 @@ def screensizeGrab():
     screensizeWidth = tk.winfo_width()
     tk.after(100, screensizeGrab)
 tk.after(100, screensizeGrab)
-sidebar = tkinter.Frame(tk, width=30,height=400, borderwidth=4, bg="grey")
-sidebar.pack(side="right")
 
+#Creates Sidebar
+sidebar = tkinter.Frame(tk, width=300,height=400, borderwidth=4, bg="grey")
+sidebar.pack(side="right")
+#
+# Creates updating variables
 def updates():
     sidebar.config(height=screensizeHeight)
     tk.after(1,updates)
 tk.after(101, updates)
+
+#Creates database
+def databaseCreation():
+    connection = sql.connect("clients.db")
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS clients(firstName TEXT, lastName TEXT, email TEXT, Topics TEXT)")
+databaseCreation()
 tk.mainloop()
